525f54b508959fd21f71cadf02cf43702debb672
saifurrahman1193 <saifur.rahman1193@gmail.com>
2025-11-17 16:58:34 +0600

    updated

--- controllers/face_detection_controller.py
+++ controllers/face_detection_controller.py
@@ -3,7 +3,7 @@ import sys
 import json
 import pandas as pd
 import numpy as np
-from utils.utils import clean_numeric_list, select_best_polynomial_model_ridge, forward_fill, backward_fill
+from utils.utils import clean_numeric_list
 
 async def handle_face_detection():
     return 'Hello from face detection controller'
